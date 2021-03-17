23becd0db4b2defae43bc182c6917c05225ef3db
Sam Herring <sam_student@idcare.org>
2021-03-17 14:34:04 +1000

    fix(frontend): :wastebasket: removed custom colors; we're not using them yet!

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -9,14 +9,8 @@ module.exports = {
       spacing: {
         '100': '30rem',
         '768': '768px'
-      }
+      },
     },
-    colors: {
-      'idc-green': '#7cb342',
-      'idc-blue': '#4598d7',
-      'action-danger': '#ff1744',
-      'action-sucess': '#29d14b',
-    }
   },
   variants: {
     extend: {},
