bffba37a90a067fbb05a34ab8a2c0e6506373030
Sam Herring <sam_student@idcare.org>
2021-03-17 10:01:35 +1000

    feat: :building_construction: added body-parser requirement

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,26 +1,31 @@
-/* eslint-disable prettier/prettier */
+//Import Environment Vars
 import dotenv from 'dotenv';
 dotenv.config();
-
-import express from 'express';
-import { Application, Request, Response } from 'express';
-
-import morgan from 'morgan';
-import helmet from 'helmet';
-
-
-//import knexfile from '../knexfile';
-const app: Application = express();
 const port = process.env.PORT || 3000;
 
+//Import Express
+import express from 'express';
+import { Application, Request, Response } from 'express';
+const app: Application = express();
+
+//Import Middleware
+import morgan from 'morgan';
+import helmet from 'helmet';
+import bodyParser from 'body-parser';
+
+
 // Empty Promise to mock connection to a DB
+//TODO Connect to the actual DB
 const data = Promise.resolve()
 
+//Begin Processing 
 Promise.resolve(data).then(() => {
   app.disable('etag');
   app
     .use(helmet())
     .use(morgan('dev'))
+    .use(bodyParser.json())
+    .use(bodyParser.urlencoded({ extended: true }))
 
   app
     .get('/test', (_req: Request, res: Response) => (res.send()))
