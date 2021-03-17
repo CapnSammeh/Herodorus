93e34341f781e4addae53e84c8d83ea685b5f735
Sam Herring <sam_student@idcare.org>
2021-03-17 14:56:11 +1000

    feat(frontend): :lipstick: added album cover component

---
+++ frontend/src/components/AlbumCover/AlbumCover.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const AlbumCover: React.FC = () => (
+  <div className="flex">
+    <img src="https://i.scdn.co/image/89b92c6b59131776c0cd8e5df46301ffcf36ed69" id="AlbumCover" className="content-center flex-1 object-none p-10"></img>
+  </div>
+);
+
+export default AlbumCover;
