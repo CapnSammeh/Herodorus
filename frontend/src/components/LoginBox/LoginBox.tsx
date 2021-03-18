c26e5f5efb7a1a85e981c61d4f845d75f6843fe6
Sam Herring <sam_student@idcare.org>
2021-03-18 15:45:41 +1000

    fix(frontend): :bug: removed an unneccessary module call

--- frontend/src/components/LoginBox/LoginBox.tsx
+++ frontend/src/components/LoginBox/LoginBox.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMusic } from '@fortawesome/free-solid-svg-icons';
-import Redirect from 'react-router-dom';
 
 const LoginBox: React.FC = () => {
   return (
