86b942a44c4210046d5237b1669680efafa1a166
Sam Herring <sam_student@idcare.org>
2021-04-14 23:31:40 +1000

    feat(frontend): :sparkles: updated albumcover to use axios to get the currentsongimage from the backend!

--- frontend/src/components/AlbumCover/AlbumCover.tsx
+++ frontend/src/components/AlbumCover/AlbumCover.tsx
@@ -2,6 +2,17 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Box } from '@material-ui/core';
 import AlbumImage from '../AlbumImage/AlbumImage'
+import axios from 'axios';
+
+const sendGetRequest = () => {
+  return axios({
+    url: '/api/currentsongimage',
+    method: 'GET'
+  }).then(response => {
+    console.log(response.data);
+    return response.data;
+  }) 
+}
 
 const useStyles = makeStyles(() => ({
   box: {
@@ -15,10 +26,14 @@ const useStyles = makeStyles(() => ({
 }));
 
 const AlbumCover: React.FC = () => {
+  const [image, setImage] = React.useState("");
+  React.useEffect(() => {
+    sendGetRequest().then(data => setImage(data));
+  });
   const classes = useStyles();
   return (
     <Box className={classes.box}>
-      <AlbumImage imgsrc="https://i.scdn.co/image/07c323340e03e25a8e5dd5b9a8ec72b69c50089d"/>
+      <AlbumImage imgsrc={image} />
     </Box>
   )
 };
