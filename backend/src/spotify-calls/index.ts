5a558ef2b5494f46fdd6a856d7ed5851d838ab93
Sam Herring <sam_student@idcare.org>
2021-04-21 19:00:34 +1000

    fix(frontend): :ambulance: getCurrentSong doesn't crash teh whole server now

--- backend/src/spotify-calls/index.ts
+++ backend/src/spotify-calls/index.ts
@@ -59,44 +59,50 @@ export async function getTenSongs(user: Express.User) {
 export async function getCurrentSong(user: Express.User) {
     //Good example of Long-Polling
     const dbUser: UserDetail = user as UserDetail;
-    const request = await fetch(
-        'https://api.spotify.com/v1/me/player', {
-        method: "GET",
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + dbUser.access_token,
+    (async () => {
+        try {
+            const request = await fetch(
+                'https://api.spotify.com/v1/me/player', {
+                method: "GET",
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + dbUser.access_token,
+                }
+            })
+            if (request.status == 502) {
+                console.log("Connection Timeout");
+                await getCurrentSong(user);
+            } else if (request.status == 401) {
+                console.log("401 Error");
+                const userRepository = getCustomRepository(UserRepository);
+                userRepository.updateAccessToken(dbUser.user_id, dbUser.access_token);
+                //TODO: Need to refresh the user's access token, then recall;
+                await new Promise(resolve => setTimeout(resolve, 1000));
+                await getCurrentSong(user);
+            } else {
+                const songData = await request.json();
+                const songInformation = await spotifyGetTrack(dbUser.access_token, songData.item.id);
+                const song: Omit<SongDetail, 'song_id'> = {
+                    album_art: songInformation.album.images[0].url,
+                    spotify_song_id: songInformation.id,
+                    artist_name: songInformation.album.artists[0].name,
+                    album_name: songInformation.album.name,
+                    song_title: songInformation.name,
+                    played_datetime: new Date(),
+                    user_: dbUser
+                }
+                //TODO: This needs to be refactored
+                getManager().createQueryBuilder()
+                    .insert()
+                    .into(SongDetail)
+                    .values([song])
+                    .onConflict(`DO NOTHING`)
+                    .execute();
+                await getCurrentSong(user);
+            }
+        } catch (e) {
+            await new Promise(resolve => setTimeout(resolve, 1000));
+            await getCurrentSong(user);
         }
-    }
-    );
-    if (request.status == 502) {
-        console.log("Connection Timeout");
-        await getCurrentSong(user);
-    } else if (request.status == 401) {
-        console.log("401 Error");
-        const userRepository = getCustomRepository(UserRepository);
-        userRepository.updateAccessToken(dbUser.user_id, dbUser.access_token);
-        //TODO: Need to refresh the user's access token, then recall;
-        await new Promise(resolve => setTimeout(resolve, 1000));
-        await getCurrentSong(user);
-    } else {
-        const songData = await request.json();
-        const songInformation = await spotifyGetTrack(dbUser.access_token, songData.item.id);
-        const song: Omit<SongDetail, 'song_id'> = {
-            album_art: songInformation.album.images[0].url,
-            spotify_song_id: songInformation.id,
-            artist_name: songInformation.album.artists[0].name,
-            album_name: songInformation.album.name,
-            song_title: songInformation.name,
-            played_datetime: new Date(),
-            user_: dbUser
-        }
-        //TODO: This needs to be refactored
-        getManager().createQueryBuilder()
-            .insert()
-            .into(SongDetail)
-            .values([song])
-            .onConflict(`DO NOTHING`)
-            .execute();
-        await getCurrentSong(user);
-    }
+    })();
 }
