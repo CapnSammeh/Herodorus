83558ef4b97d7a4d501e6bd4dd459c7fd3278b7b
Sam Herring <sam_student@idcare.org>
2021-04-21 14:57:09 +1000

    feat(frontend): :sparkles: used material-ui-image to make the image load nicely when the page is refresh

--- frontend/src/components/AlbumImage/AlbumImage.tsx
+++ frontend/src/components/AlbumImage/AlbumImage.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core';
+import Image from 'material-ui-image';
+
 
 interface AlbumImageProps {
   imgsrc: string
@@ -13,21 +15,22 @@ const useStyles = makeStyles(() => ({
   },
 }))
 
-function flipImage(e:React.MouseEvent){
+function flipImage(e: React.MouseEvent) {
   //TODO: Put some fancy transform here so the element flips
   alert("You clicked me!" + e.type);
 }
 
 const AlbumImage: React.FC<AlbumImageProps> = (props) => {
   const { imgsrc } = props;
-  const classes = useStyles();
   return (
-    <img
-       
-      className = {classes.img} 
-      src = {imgsrc} 
-      onClick = {e => flipImage(e)}
-    />
+    <div style={{width: '600px', height: '600px'}}>
+      <Image
+        src={imgsrc}
+        style={useStyles}
+        onClick={e => flipImage(e)}
+        disableSpinner
+      />
+    </div>
   )
 };
 
