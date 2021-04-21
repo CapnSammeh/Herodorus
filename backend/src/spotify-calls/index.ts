141ccca45d43a7393714ed1d8520fc4ff86041e2
Sam Herring <sam_student@idcare.org>
2021-04-21 11:42:52 +1000

    refactor(backend): :recycle: refactored spotify-calls to be more consistent with typeorm entity management

--- backend/src/spotify-calls/index.ts
+++ backend/src/spotify-calls/index.ts
@@ -1,30 +1,10 @@
 import fetch from 'node-fetch';
 import { SongDetail } from '../db/entity/SongDetail/SongDetail';
 import { UserDetail } from "../db/entity/UserDetail/UserDetail";
-import { getConnection } from 'typeorm';
+import { getManager } from 'typeorm';
 
 export async function getTenSongs(user: Express.User) {
     const dbUser: UserDetail = user as UserDetail;
-    //Clear the songs in the DB for the current user
-    //TODO: Fix this up, notes below
-    /*
-        Not sure this is the best way to go about this
-
-        Actually I'm almost certain it isn't.
-
-        The idea here is that the user should have 10 songs loaded up, ready to go, that way if they're not currently playing anything, we can fall back to cycling through those. 
-        By deleting what's in the db, we give ourselves the chance to populate some more recent songs so we're not showing anything super stale, but again, is that the idea here?
-        The arbitriary 10-song-limit was just something I considered appropriate, could be modified. 
-
-        
-    */
-    const db = await getConnection();
-    db.createQueryBuilder()
-        .delete()
-        .from(SongDetail)
-        .where("user_ = :user", { user: dbUser.user_id })
-        .execute();
-
     const songs = await fetch(
         "https://api.spotify.com/v1/me/player/recently-played?limit=10", {
         method: "GET",
@@ -47,11 +27,27 @@ export async function getTenSongs(user: Express.User) {
             user_: dbUser
         }
 
-        db.createQueryBuilder()
+        //TODO: This needs to be refactored
+        getManager().createQueryBuilder()
             .insert()
             .into(SongDetail)
             .values([song])
             .onConflict(`DO NOTHING`)
             .execute();
     }
+}
+
+export async function getCurrentSong(user: Express.User) {
+    const dbUser: UserDetail = user as UserDetail;
+    const song = await fetch(
+        'https://api.spotify.com/v1/me/player', {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + dbUser.access_token,
+            }
+        }
+    ).then(res => res.json());
+
+    console.log(song);
 }
