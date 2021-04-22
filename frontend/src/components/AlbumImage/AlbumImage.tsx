171b7f0025a85f8380b0815d7a1f849b15eacdb7
Sam Herring <sam_student@idcare.org>
2021-04-22 12:32:50 +1000

    fix(frontend): :recycle: refactored to use react-spring for animation

--- frontend/src/components/AlbumImage/AlbumImage.tsx
+++ frontend/src/components/AlbumImage/AlbumImage.tsx
@@ -1,37 +1,39 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core';
-import Image from 'material-ui-image';
-
+import React, { useEffect, useState } from 'react';
+import { useSpring, animated } from 'react-spring';
 
 interface AlbumImageProps {
   imgsrc: string
 }
 
-const useStyles = makeStyles(() => ({
-  img: {
-    margin: 'auto',
-    objectFit: 'contain',
-    userSelect: 'none',
-  },
-}))
-
-function flipImage(e: React.MouseEvent) {
-  //TODO: Put some fancy transform here so the element flips
-  alert("You clicked me!" + e.type);
-}
-
 const AlbumImage: React.FC<AlbumImageProps> = (props) => {
-  const { imgsrc } = props;
+  const [flipped, setFlipped] = useState(false);
+  const [transform, setTransformation] = useSpring(() => ({
+    transform: `perspective(600px) rotateX(${flipped ? 180 : 0}deg)`,
+    config: { mass: 5, tension: 500, friction: 80 },
+  }))
+
+  const [fade, setFade] = useSpring(() => ({ opacity: 0 }));
+  const [img, setImage] = useState("");
+  useEffect(() => {
+    setImage(props.imgsrc);
+    setFade.start({
+      to: { opacity: 1 },
+      from: { opacity: 0 },
+      config: {
+        duration: 1500
+      }
+    });
+  }, [props.imgsrc, setFade]);
+
+  console.log(props);
   return (
-    <div style={{width: '600px', height: '600px'}}>
-      <Image
-        src={imgsrc}
-        style={useStyles()}
-        onClick={e => flipImage(e)}
-        disableSpinner
-      />
+    // <animated.img style={animation} src={props.imgsrc} onClick={e => flipImage(e)} />
+    <div onClick={() => setTransformation(state => !state)}>
+      <animated.div style={fade}>
+        <img src={img} />
+      </animated.div>
     </div>
   )
-};
+}
 
 export default AlbumImage;
