b053bfc3919608ad465af70ed6cb77630e9ba2dc
Sam Herring <sam_student@idcare.org>
2021-04-22 12:35:34 +1000

    fix(frontend): :fire: removed onClick function/transform; it's too early for that

--- frontend/src/components/AlbumImage/AlbumImage.tsx
+++ frontend/src/components/AlbumImage/AlbumImage.tsx
@@ -6,12 +6,6 @@ interface AlbumImageProps {
 }
 
 const AlbumImage: React.FC<AlbumImageProps> = (props) => {
-  const [flipped, setFlipped] = useState(false);
-  const [transform, setTransformation] = useSpring(() => ({
-    transform: `perspective(600px) rotateX(${flipped ? 180 : 0}deg)`,
-    config: { mass: 5, tension: 500, friction: 80 },
-  }))
-
   const [fade, setFade] = useSpring(() => ({ opacity: 0 }));
   const [img, setImage] = useState("");
   useEffect(() => {
@@ -28,7 +22,7 @@ const AlbumImage: React.FC<AlbumImageProps> = (props) => {
   console.log(props);
   return (
     // <animated.img style={animation} src={props.imgsrc} onClick={e => flipImage(e)} />
-    <div onClick={() => setTransformation(state => !state)}>
+    <div onClick={() => alert("You clicked me!")}>
       <animated.div style={fade}>
         <img src={img} />
       </animated.div>
