ff36b9a1d7df5a529b75fa1f9f80dd88764021cb
Sam Herring <sam_student@idcare.org>
2021-05-01 15:05:28 +1000

    feat(frontend): :sparkles: artpage now redirects the user back to the login page if they're not auth'd to the server

--- frontend/src/pages/ArtPage/ArtPage.tsx
+++ frontend/src/pages/ArtPage/ArtPage.tsx
@@ -1,12 +1,40 @@
 import React from 'react';
 // import AlbumInfo from "@components/AlbumInfo/AlbumInfo";
 import AlbumCover from "@components/AlbumCover/AlbumCover";
+import axios from 'axios';
+import { Redirect } from 'react-router-dom';
 
-const ArtPage: React.FC = () => (
-  <>
-    <AlbumCover />
-    {/* <AlbumInfo /> */}
-  </>
-);
+const ArtPage: React.FC = () => {
+  const [auth, setAuth] = React.useState(true);
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios("/api/currentsongimage",
+        {
+          validateStatus: function () {
+            return true; // default
+          }
+        })
+        .then(response => response.status)
+      if (result == 200) {
+        setAuth(true);
+      } else {
+        setAuth(false);
+      }
+    }
+    fetchData();
+  }, []);
+
+  if(auth == true){
+    return (
+      <AlbumCover />
+    );
+  } else {
+    return (
+      <Redirect to="/login" />
+    )
+  }
+
+}
 
 export default ArtPage;
