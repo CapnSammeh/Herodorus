2afa30bd0b9be0053c735954bb5fa3d40f751435
Sam Herring <sam_student@idcare.org>
2021-05-01 14:37:41 +1000

    feat(frontend): :sparkles: login button is now reactive based on the user's auth'd status

--- frontend/src/components/LoginButton/LoginButton.tsx
+++ frontend/src/components/LoginButton/LoginButton.tsx
@@ -1,10 +1,39 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import axios from 'axios';
 
+const LoginButton: React.FC = () => {
+  const [buttonText, setButtonText] = React.useState("");
+  const [href, setHref] = React.useState("");
 
-const LoginButton: React.FC = () => (
-  <div>
-    LoginButton Component
-  </div>
-);
+  React.useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios("/api/currentsongimage",
+        {
+          validateStatus: function () {
+            return true; // default
+          }
+        })
+        .then(response => response.status)
+        .catch(e => console.error(e));
+      console.log(result);
+      if (result == 403) {
+        console.log("Logged out!");
+        setButtonText("Login");
+        setHref("/login");
+      } else {
+        //We're auth'd, so show the logout button
+        console.log("Logged in!");
+        setButtonText("Logout");
+        setHref("/api/logout");
+      }
+    };
+    fetchData();
+  }, [setButtonText]);
+
+  return (
+    <Button color="inherit" href={href}>{buttonText}</Button>
+  )
+};
 
 export default LoginButton;
