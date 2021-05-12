745c8c03c7750aa538e5871da638b0a4fffc3551
Sam Herring <samherring111@gmail.com>
2021-05-12 11:15:08 +1000

    ci: :construction_worker: updated template for component to appropriate return a value

--- frontend/templates/components/TemplateName.tsx
+++ frontend/templates/components/TemplateName.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import "./TemplateName.module.css";
 
-const TemplateName: React.FC = () => {   
-    return ()
+const TemplateName: React.FC = () => {
+    return ();
 }
 
 export default TemplateName;
