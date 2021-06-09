888ee832f90db5f47b52c8b2d01c3cb90c63c553
Sam Herring <samherring111@gmail.com>
2021-06-09 12:48:49 +1000

    feat(frontend): :bug: changed max width of image element

--- frontend/src/components/AlbumImage/AlbumImage.tsx
+++ frontend/src/components/AlbumImage/AlbumImage.tsx
@@ -12,7 +12,7 @@ const AlbumImage: React.FC<AlbumImageProps> = (props) => {
   const matches = useMediaQuery(theme.breakpoints.down('sm'));
   console.log(matches);
 
-  const [fade, setFade] = useSpring(() => ({ opacity: 0, width:"80%", maxWidth:"60%" }));
+  const [fade, setFade] = useSpring(() => ({ opacity: 0, width:"130%"}));
   const [img, setImage] = useState("");
 
   useEffect(() => {
